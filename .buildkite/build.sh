23c0185b967dcf8b5e59ee585436ad18d770ffa7
Lachlan Donald <lachlan@ljd.cc>
2017-07-16 22:37:15 +1000

    Upload install.log on failure

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -4,4 +4,8 @@ set -euo pipefail
 image="$1"
 
 echo "--- Building $image"
-make "$image"
+if ! make "$image" ; then
+  buildkite-agent artifact upload /var/log/install.log
+  echo "Failed to build $image"
+  exit 1
+fi
