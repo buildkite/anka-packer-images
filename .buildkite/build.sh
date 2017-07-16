3adea3b0e6f2f35f053b3d60a604fed64a75861e
Lachlan Donald <lachlan@ljd.cc>
2017-07-16 22:43:02 +1000

    Check shasum of installer

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -8,9 +8,13 @@ function upload_install_log() {
 image="$1"
 
 if [[ $image == "macos-10.12" ]] ; then
+  echo "--- Installing error trap for install.log"
   echo "" > /var/log/install.log
   trap upload_install_log ERR
 fi
+
+echo "--- Calculating installer checksums"
+shasum /Applications/Install*OS*.app/Contents/SharedSupport/InstallESD.dmg
 
 echo "--- Building $image"
 make "$image"
