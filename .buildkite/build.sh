5671343f2f396d207819d06ea6756e7a1761866c
Lachlan Donald <lachlan@ljd.cc>
2017-07-17 18:17:28 +1000

    Add xcode step

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -10,10 +10,11 @@ image="$1"
 if [[ $image == "macos-10.12" ]] ; then
   echo "--- Installing error trap for install.log"
   trap upload_install_log ERR
-fi
 
-echo "--- Calculating installer checksums"
-shasum /Applications/Install*OS*.app/Contents/SharedSupport/InstallESD.dmg
+  echo "--- Calculating installer checksums"
+  echo "Compare to https://github.com/notpeter/apple-installer-checksums"
+  shasum /Applications/Install*OS*.app/Contents/SharedSupport/InstallESD.dmg
+fi
 
 echo "--- Building $image"
 make "$image"
